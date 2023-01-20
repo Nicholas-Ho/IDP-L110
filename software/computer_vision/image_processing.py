36e05bd200051d8852c77e9e9b66146cbff4891c
Nicholas Ho <j.xiong@live.com>
2023-01-20 17:09:49 +0000

    Started on block detection

--- software/computer_vision/image_processing.py
+++ software/computer_vision/image_processing.py
@@ -11,6 +11,12 @@ low_thresh = 50
 hi_thresh = thresh_ratio*low_thresh
 binary_thresh_j = 15
 
+# Block Detection Parameters
+red_low_1 = (0,75,150)
+red_hi_1 = (5,255,255)
+red_low_2 = (175,75,150)
+red_hi_2 = (180,255,255)
+
 
 def _process_img(img):
     # Blurring to smear non-targets
@@ -19,19 +25,10 @@ def _process_img(img):
     # Edge detection
     img = cv2.Canny(img, low_thresh, hi_thresh)
 
-    plt.imshow(img, cmap='gray')
-    plt.show()
-
     # Blurring, Thresholding and Skeletonization
     img = cv2.blur(img, (10,10))
-    plt.imshow(img, cmap='gray')
-    plt.show()
     ret, img = cv2.threshold(img, binary_thresh_j, 255, cv2.THRESH_BINARY)
-    plt.imshow(img, cmap='gray')
-    plt.show()
     img = skeletonize(img//255).astype(int)
-    plt.imshow(img, cmap='gray')
-    plt.show()
 
     return img
 
@@ -68,22 +65,39 @@ def find_junctions(img):
 
     return junctions
 
-def find_red():
-    pass
+def find_red(img):
+    rgb_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    ret, rgb_img = cv2.threshold(rgb_img, 25, 255, 0)
+    contours, hierarchy = cv2.findContours(rgb_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    objects = cv2.drawContours(np.zeros(img.shape, dtype=np.uint8), contours, -1, (255, 255, 255), 2)
+    plt.imshow(objects)
+    plt.show()
 
-img_path = 'sample_picture_bright_20.01.2023.png'
+    # Convert to HSV
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    mask1 = cv2.inRange(hsv_img, red_low_1, red_hi_1)
+    mask2 = cv2.inRange(hsv_img, red_low_2, red_hi_2)
+
+    mask = cv2.bitwise_or(mask1, mask2)
+    mask = cv2.bitwise_and(mask1, objects)
+    plt.imshow(mask)
+    plt.show()
+
+    
+
+# img_path = 'sample_picture_bright_20.01.2023.png'
 # img_path = 'sample_picture_dark_19.01.2023.png'
+img_path = 'sample_picture_block_20.01.2023.png'
 
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-# plt.imshow(img)
-# plt.show()
 
 # Find junctions
 junctions = find_junctions(img)
 for junction in junctions:
     img = cv2.circle(img, junction, radius=15, color=(255, 0, 0), thickness=-1)
+
+# find_red(img)
 
 plt.imshow(img)
 plt.show()
