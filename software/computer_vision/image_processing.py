fe6a2d86d0106e0a68b0e6b3879614319a1eb725
Nicholas Ho <j.xiong@live.com>
2023-01-20 14:51:13 +0000

    Optimised junction detection

--- software/computer_vision/image_processing.py
+++ software/computer_vision/image_processing.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 from skimage.morphology import skeletonize
 from sklearn.cluster import AgglomerativeClustering
+from scipy.ndimage import convolve
 import numpy as np
 
 # Junction Detection Parameters
@@ -12,13 +13,25 @@ binary_thresh_j = 15
 
 
 def _process_img(img):
+    # Blurring to smear non-targets
+    img = cv2.blur(img, (4,4))
+
     # Edge detection
     img = cv2.Canny(img, low_thresh, hi_thresh)
 
+    plt.imshow(img, cmap='gray')
+    plt.show()
+
     # Blurring, Thresholding and Skeletonization
     img = cv2.blur(img, (10,10))
+    plt.imshow(img, cmap='gray')
+    plt.show()
     ret, img = cv2.threshold(img, binary_thresh_j, 255, cv2.THRESH_BINARY)
+    plt.imshow(img, cmap='gray')
+    plt.show()
     img = skeletonize(img//255).astype(int)
+    plt.imshow(img, cmap='gray')
+    plt.show()
 
     return img
 
@@ -28,24 +41,18 @@ def find_junctions(img):
     max_val = img.max()
     img = img//max_val
 
-    junctions = []
-
     h, w = img.shape
 
+    kernel = [[1, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]]
+    # Since the image should only contain 1 pixel-wide skeletons, any activated pixel that has more than 2 neighbours is most likely a junction.
     # We're not expecting the path to be at the very edges of the image, so it's okay to leave out the edges
-    for i in range(1,h-1):
-        for j in range(1, w-1):
-            # Since the image should only contain 1 pixel-wide skeletons, any activated pixel that has more than 2 neighbours is most likely a junction.
-            if img[i][j] == 1:
-                if (img[i-1][j-1] 
-                        + img[i-1][j]
-                        + img[i-1][j+1]
-                        + img[i][j-1]
-                        + img[i][j+1]
-                        + img[i+1][j-1]
-                        + img[i+1][j]
-                        + img[i+1][j+1]) > 2: # More than 2 white pixels out of 8
-                    junctions.append((j,i))
+    junction_mat = convolve(img, kernel, mode='constant', cval=0).astype(np.uint8)
+    ret, junction_mat = cv2.threshold(junction_mat, 3, 255, cv2.THRESH_BINARY)
+
+    junctions = np.array(list(zip(*(np.nonzero(junction_mat)))))
+    junctions = np.flip(junctions, axis=1)
 
     # Clustering
     ward = AgglomerativeClustering(n_clusters=None, linkage='ward', distance_threshold=50)
@@ -61,9 +68,11 @@ def find_junctions(img):
 
     return junctions
 
+def find_red():
+    pass
 
-# img_path = 'sample_picture_bright_20.01.2023.png'
-img_path = 'sample_picture_dark_19.01.2023.png'
+img_path = 'sample_picture_bright_20.01.2023.png'
+# img_path = 'sample_picture_dark_19.01.2023.png'
 
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
