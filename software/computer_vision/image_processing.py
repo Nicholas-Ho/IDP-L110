344a2101d4dc35d085d0cb5956ca17aa73db13cf
Nicholas Ho <j.xiong@live.com>
2023-01-23 17:28:25 +0000

    Added junction-block linkage

--- software/computer_vision/image_processing.py
+++ software/computer_vision/image_processing.py
@@ -12,13 +12,10 @@ hi_thresh = thresh_ratio*low_thresh
 binary_thresh_j = 15
 
 # Block Detection Parameters
-red_low_1 = (0,75,150)
-red_hi_1 = (5,255,255)
-red_low_2 = (175,75,150)
-red_hi_2 = (180,255,255)
-
 cyan_low = (160, 70, 50)
 cyan_high = (200, 255, 255)
+
+crop = {'y': (50, 700), 'x': (200, 400)}
 
 def _process_img(img):
     # Blurring to smear non-targets
@@ -54,7 +51,7 @@ def find_junctions(img):
     junctions = np.flip(junctions, axis=1)
 
     # Clustering
-    ward = AgglomerativeClustering(n_clusters=None, linkage='ward', distance_threshold=50)
+    ward = AgglomerativeClustering(n_clusters=None, linkage='ward', distance_threshold=30)
     labels = ward.fit_predict(junctions)
     clusters = {}
     for junction, label in zip(junctions, labels):
@@ -68,41 +65,66 @@ def find_junctions(img):
     return junctions
 
 def find_red(img):
-    #Cropping left side of image to isolate junctions with block
-    zoomed_img = img[50:700, 200:400]
-    #Inverting the image and looking for the colour cyan -- same as looking for red in non-inverted space
+    # Cropping left side of image to isolate junctions with block
+    zoomed_img = img[crop['y'][0]:crop['y'][1], crop['x'][0]:crop['x'][1]]
+
+    # Inverting the image and looking for the colour cyan -- same as looking for red in non-inverted space
     hsv_img = cv2.cvtColor(zoomed_img, cv2.COLOR_RGB2HSV)
     mask = cv2.inRange(hsv_img, cyan_low, cyan_high)
 
-    #Looking for the largest cyan contour -- this should correspond to the block
+    # Looking for the largest cyan contour -- this should correspond to the block
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) > 0:
         max_contour = max(contours, key=cv2.contourArea)
-        x,y,w,h = cv2.boundingRect(max_contour)
-        cv2.rectangle(zoomed_img,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.putText(zoomed_img, "Red Block", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
 
-    #Converting back to BGR space
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        # Find centre of contour
+        M = cv2.moments(max_contour)
+        if M['m00'] != 0:
+            cx = int(M['m10']/M['m00']) + crop['x'][0] # Converting coordinates back to uncropped
+            cy = int(M['m01']/M['m00']) + crop['y'][0]
 
-    cv2.imshow("Mask", img)
-    cv2.waitKey(0)
+    return (cx, cy), max_contour
+
+def find_block_junctions(junctions, blocks):
+    def euclidean_dist(pt1, pt2):
+        return np.sqrt((pt1[0]-pt2[0])**2 + (pt1[1]-pt2[1])**2)
+
+    block_junctions = []
+    for block in blocks:
+        block_junctions.append((min(junctions, key=lambda x: euclidean_dist(x, block)), block)) # (junction, block)
+
+    return block_junctions
 
 
-
-# img_path = 'sample_picture_bright_20.01.2023.png'
-# img_path = 'sample_picture_dark_19.01.2023.png'
-img_path = 'sample_picture_block_20.01.2023.png'
+img_path = 'sample_picture_block1_20.01.2023.png'
 
 img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # Find junctions
-# junctions = find_junctions(img)
-# for junction in junctions:
-#     img = cv2.circle(img, junction, radius=15, color=(255, 0, 0), thickness=-1)
+junctions = find_junctions(img)
 
-find_red(img)
+# Draw junctions
+for junction in junctions:
+    img = cv2.circle(img, junction, radius=5, color=(255, 0, 0), thickness=-1)
 
-# plt.imshow(img)
-# plt.show()
+# Detect red block
+block_centre, contour = find_red(img)
+
+# Draw red block
+x,y,w,h = cv2.boundingRect(contour)
+zoomed_img = img[crop['y'][0]:crop['y'][1], crop['x'][0]:crop['x'][1]]
+cv2.rectangle(zoomed_img,(x,y),(x+w,y+h),(0,255,0),2)
+cv2.putText(zoomed_img, "Red Block", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+
+# Find junctions closest to the block
+block_junctions = find_block_junctions(junctions, [block_centre])
+
+# Draw junctions
+for junction in block_junctions:
+    img = cv2.circle(img, junction[0], radius=10, color=(0, 255, 0), thickness=-1)
+
+# Show final image
+img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # Converting back to BGR space
+cv2.imshow("Mask", img)
+cv2.waitKey(0)
