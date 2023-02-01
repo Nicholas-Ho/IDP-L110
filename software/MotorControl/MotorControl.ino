f2375115a5e8f488aa175675a323d748ce56d81f
Nicholas Ho <j.xiong@live.com>
2023-02-01 10:28:18 +0000

    Initial commit

--- software/MotorControl/MotorControl.ino
+++ software/MotorControl/MotorControl.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_MotorShield.h>
 #include "LineFollower.h"
+#include <NewPing.h>
 
 //Instantiate MotorShield object
 Adafruit_MotorShield motor_shield = Adafruit_MotorShield(0x60);
@@ -23,6 +24,20 @@ uint8_t maxPower = 220; // 255 is too much
 //Assign line sensor pins (left to right)
 int linePins[4] = {4, 5, 6, 7};
 int lineReadings[4] = {0, 0, 0, 0};
+
+//Ultrasonic
+int trigPin = 1;
+int echoPin = 0;
+int maxDist = 10; //In cm
+
+NewPing sonar(trigPin, echoPin, maxDist);
+
+int blockState = 0; // State of using ultrasonic to detect a block
+int slowRatio = 3; // Ratio to slow down the motors by if block is detected (eg if slowRatio is 3, slow down by a factor of 3)
+
+//Ultrasonic intervals
+int uSonicInterval = 50; // In milliseconds
+long prevMillis = 0;
 
 void setup() 
 {
@@ -57,6 +72,7 @@ int printCounter = 0;
 void loop() 
 { 
   String readingPrint = "";
+
   // Read line sensors
   for(int i=0; i < 4; i++) {
     lineReadings[i] = digitalRead(linePins[i]);
@@ -69,7 +85,43 @@ void loop()
     Serial.println("Error: Line Readings not available");
     while(1);
   }
+
   controller.control(lineReadings); //left and right motor proportions are set now
+
+  // Check ultrasonic
+  long currMillis = millis();
+  if(currMillis - prevMillis >= uSonicInterval) {
+    int dist = sonar.ping_cm();
+    if(dist <= 8) {
+      // Something is within 7 cm
+      blockState = 1;
+    } else if(dist <= 4) {
+      // Something is within 4 cm
+      // TODO: Replace below code with block grabbing code
+      blockState = 2;
+    } else {
+      blockState = 0;
+    }
+  }
+
+  switch(blockState) {
+    case 0:
+      break;
+    
+    case 1:
+      leftMotorProportion /= slowRatio;
+      rightMotorProportion /= slowRatio;
+      break;
+
+    case 2:
+      // TODO: Replace with block-grabbing code
+      leftMotorPropotion = 0;
+      rightMotorProportion = 0;
+
+    default:
+      Serial.println("Invalid block state.");
+      break;
+  }
   
   //Protecting against invalid motor proportions that would cause motor speeds to exceed the max
   if (leftMotorProportion > 1.0)  {leftMotorProportion = 1;}
@@ -79,7 +131,7 @@ void loop()
   bool leftSign = leftMotorProportion >= 0 ? 1 : 0; //1 corresponds to a positive speed (forwards) and 0 to a negative speed (backwards)
   bool rightSign = rightMotorProportion >= 0 ? 1: 0;
 
-  // //Setting the motor speeds based on proportions
+  //Setting the motor speeds based on proportions
   int leftMotorSpeed = (int) (fabs(leftMotorProportion)*maxPower);
   int rightMotorSpeed = (int) (fabs(rightMotorProportion)*maxPower); 
 
