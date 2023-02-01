c818d5f33d4d37d354585ac968325998f9c24310
Nicholas Ho <j.xiong@live.com>
2023-02-01 09:16:24 +0000

    Ultrasoinc experiment (not working)

--- electronics/ultrasonic_distance/ultrasonic_distance.ino
+++ electronics/ultrasonic_distance/ultrasonic_distance.ino
@@ -1,10 +1,18 @@
 // Ultrasonic sensor (basic distance return to serial port)
 
-int trigPin = 1;
-int echoPin = 0;
+int trigPin = 3;
+int echoPin = 2;
 
 unsigned long prevMicros = 0;
-const long intervals[2] = {2, 12}; // Note: intervals from start of cycle! So between the first and second events there are 10 seconds.
+const long intervals[2] = {10000, 60000}; // Note: intervals from start of cycle! So between the first and second events there are 10 seconds.
+int duration = 0;
+unsigned long startTimeMicros = 0;
+
+// For asynchronous reading of the pulse. Replaces the synchronous pulseIn()
+// State 0: To set the trigger to LOW
+// State 1: To start timing when Echo Pin is HIGH
+// State 2: To stop timing when Echo Pin is LOW
+int state = 0;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
@@ -13,40 +21,66 @@ void setup() {
 }
 
 void loop() {
-  int distance = detectUltrasonic();
-
-  // Print to Serial Monitor, distance in cm
-  Serial.print("\nDistance: \n");
-  Serial.print(distance);
+  detectUltrasonic();
 }
 
-// Detect ultrasonic. Uses millis to ensure that it can run concurrently with other code and not interfere with delay.
+// Detect ultrasonic. Uses micros to ensure that it can run concurrently with other code and not interfere with delay.
 void detectUltrasonic() {
 
   unsigned long currMicros = micros(); // Overflows after 70 minutes
 
-  // Clear the trig pin
-  digitalWrite(trigPin, LOW);
+  // Serial.println(prevMicros);
+  Serial.println(currMicros);
 
-  if(currMicros-prevMicros >= intervals[0] && currMicros-prevMicros <= intervals[1]) { // Delay 2 microseconds to ensure trigger is cleared
+  if(state==0) {
+
+    // Clear the trig pin
+    digitalWrite(trigPin, LOW);
+
+    // Next state
+    state = 1;
+
+    Serial.println("Initialised");
+
+  } else if(currMicros-prevMicros >= intervals[0] && state==1) { // Delay 2 microseconds to ensure trigger is cleared
 
     // Set a HIGH pulse on trigger for 10 us
     digitalWrite(trigPin, HIGH);
 
-  } else if (currMicros-prevMicros >= intervals[1]) { // Delay 10 microseconds to set the trigger to LOW
-    
-    digitalWrite(trigPin, LOW);
+    // Next state
+    state = 2;
 
-    // Read echoPin time in microseconds
-    long duration = pulseIn(echoPin, HIGH);
-    
-    // Calculate distance (cm)
-    int distance = duration * 0.034/2;
+    Serial.println("Pulse");
 
-    // Set prevMicros
-    prevMicros = currMicros;
+  } else if (currMicros-prevMicros >= intervals[1] && state == 2) { // Delay 10 microseconds to set the trigger to LOW
+  
+    digitalWrite(trigPin, LOW); // End the trigPin pulse
+    Serial.println("End");
+    state = 0;
 
-    return distance;
+    attachInterrupt(echoPin, inSignal, RISING);
+    attachInterrupt(echoPin, outSignal, FALLING);
 
+    delay(1000);
   }
+}
+
+void inSignal() {
+  startTimeMicros = micros();
+}
+
+void outSignal() {
+  // If echo is LOW, get duration in microseconds
+  long currMicros = micros();
+  long duration = currMicros - startTimeMicros;
+
+  // Calculate distance (cm)
+  int distance = duration * 0.034/2;
+
+  // Set prevMicros
+  prevMicros = currMicros;
+
+  // Print to Serial Monitor, distance in cm
+  Serial.print("Distance: ");
+  Serial.println(distance);
 }
