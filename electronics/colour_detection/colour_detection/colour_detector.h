9f4174d888899cb7307472b7355ba9d65afa568b
Nicholas Ho <j.xiong@live.com>
2023-02-07 11:08:30 +0000

    Refactored code

---
+++ electronics/colour_detection/colour_detection/colour_detector.h
@@ -0,0 +1,29 @@
+#include "circular_buffer.h"
+#include <Arduino.h>
+
+class ColourDetector {
+    private:
+        // State 0: Dormant
+        // State 1: Initialising
+        // State 2: Recording
+        int state = 0;
+
+        int blueThreshold = 40; // in bits
+
+        // Checking against sample approximately 1 s ago
+        CircularBuffer pastSamples = CircularBuffer(100, true, 1023);
+
+        // Sample approximately every 10 ms
+        int interval = 10;
+        long currMillis = 0;
+        long prevMillis = 0;
+
+        // Counter, if approximately 5 seconds is up, declare red
+        int counterThresh = 500;
+        int counter = 0;
+
+    public:
+        void initialiseDetector();
+        int detectColour(int sensorVal);
+        int getState();
+}
